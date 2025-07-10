e87f9761b601b91c8f4e13887cae9ac74c6d99b2
Arya-chaurasia <aryachaurasia11@gmail.com>
2025-07-10 16:30:52 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const router = express.Router()
 
@@ -33,19 +34,59 @@ router.get("/user/connections", userAuth, async (req, res) => {
                 { fromUserId: loggedInUser, status: "accepted" }
             ]
         }).populate("fromUserId", "firstName lastName photoUrl age gender skills")
-        .populate("toUserId", "firstName lastName photoUrl age gender skills")
+            .populate("toUserId", "firstName lastName photoUrl age gender skills")
 
         const data = connectionRequests.map((row) => {
-            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
                 return row.toUserId
             }
             return row.fromUserId
         })
 
-        res.json({data})
+        res.json({ data })
 
     } catch (err) {
         res.status(400).send({ message: "Error: " + err.message })
+    }
+})
+
+
+router.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 15;
+        const skip = (page - 1) * limit
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromFeed = new Set();
+        connectionRequest.forEach((req) => {
+            hideUserFromFeed.add(req.fromUserId.toString())
+            hideUserFromFeed.add(req.toUserId.toString())
+        })
+
+        const user = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUserFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+
+            ]
+        }).select("firstName lastName photoUrl age gender skills")
+            .skip(skip)
+            .limit(limit)
+
+        res.send({data:user})
+
+    } catch (err) {
+        console.log(err, "errrrrrrrrrr")
+        res.status(400).json({ message: err.message })
     }
 })
 
