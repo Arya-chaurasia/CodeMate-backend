046ff31c9a7305c4dcd1f88b9a8c7f15e448d36e
Arya-chaurasia <aryachaurasia11@gmail.com>
2025-08-01 11:17:32 +0530

    updated db

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://localhost:27017/DevTinder")
-}
-module.exports = connectDB
+    try {
+        await mongoose.connect("mongodb+srv://arya:devTinder@cluster0.4ofip.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0", {
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
